ea292d80251d18dd8052e98cf9ab023e85a3a834
ibayramli2001 <43158991+ibayramli2001@users.noreply.github.com>
2019-04-18 14:00:45 -0400

    update

--- shotspotter/app.R
+++ shotspotter/app.R
@@ -1,13 +1,11 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(fs)
+library(tidyverse)
+
+# We downlaoded the data and saved it in the github repo. By the nature of the
+# dataset, we don't expect it to change anythime soon.
+
+data <- read_csv("st_louis_data.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
