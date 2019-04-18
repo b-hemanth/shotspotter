92de6e22554b084153b3783bbef89016791c6ddf
Hemanth Bharatha Chakravarthy <hemanthbharathachakravarthy@college.harvard.edu>
2019-04-18 14:42:39 -0400

    Shapefile initialization

--- shotspotter/app.R
+++ shotspotter/app.R
@@ -12,12 +12,39 @@ library(ggplot2)
 library(viridis)
 
 
-# We downlaoded the data and saved it in the github repo. By the nature of the
+# We downloaded the data and saved it in the github repo. By the nature of the
 # dataset, we don't expect it to change anythime soon.
 
-data <- read_csv("st_louis_data.csv")
+data <- read_csv("wash_data.csv",
+                 cols(
+                   incidentid = col_double(),
+                   latitude = col_double(),
+                   longitude = col_double(),
+                   year = col_double(),
+                   month = col_double(),
+                   day = col_double(),
+                   hour = col_double(),
+                   minute = col_double(),
+                   second = col_double(),
+                   numshots = col_double(),
+                   type = col_logical()
+                 ),
+                 col_names = TRUE
+                 )
 
-mapview(data, xcol = "Longitude", ycol = "Latitude", crs = 4269, grid = FALSE)
+data_16 <- data %>%
+  filter(year == 2016)
+
+shape_data <- st_as_sf(data_16, coords = c("longitude", "latitude"),  crs=4326)
+
+
+ggplot(data = shapes_data) +
+  geom_sf() +
+  geom_sf(data = arrest_locations) +
+  theme_map() + 
+  labs(title = "Location of Hartford Arrests",
+       subtitle = "Asian-Americans 2013 -- 2016", 
+       caption = "Source: Stanford Open Policing Project" )
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
